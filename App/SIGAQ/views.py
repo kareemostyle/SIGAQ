b57c3f27f30eb4aaeebd112424ef6556cd855450
Kareem Taleb <kmt96@case.edu>
2019-08-02 19:35:59 -0400

    Trying rendering views

--- App/SIGAQ/views.py
+++ App/SIGAQ/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World. Welcome to the index of SIGAQ.")
+    return render(request, 'SIGAQ/index.html')
