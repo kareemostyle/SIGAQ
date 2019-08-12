629b04e46222d0ac4574cf7fcc9f3e5692af83ab
kareemostyle <kmt96@case.edu>
2019-08-12 11:47:52 -0400

    MAde function to render each html file

--- App/SIGAQ/views.py
+++ App/SIGAQ/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'SIGAQ/index.html')
+
+def blog(request):
+    return render(request, 'SIGAQ/blog.html')
+
+def contact(request):
+    return render(request, 'SIGAQ/contact.html')
+
+def faq(request):
+    return render(request, 'SIGAQ/faq.html')
+
+def forum(request):
+    return render(request, 'SIGAQ/forum.html')
+
+def login(request):
+    return render(request, 'SIGAQ/login.html')
